67706dcd85d73f1904114cff8cce1b4c7d72c912
Tomas Vargas <tavargas9@gmail.com>
2023-11-21 00:27:39 -0700

    dry up code

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
 const db = require('./config/connection.js');
-const questions = require('./lib/questions.js');
-const inquirer = require('inquirer');
-const handleAnswers = require('./lib/handleAnswers.js');
 const init = require('./lib/init.js')
 
 db.connect(err => {
